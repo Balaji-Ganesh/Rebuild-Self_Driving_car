0f2f50ac0e22e4fe5972c141005c79097447cca0
Balaji-Ganesh <balganeshboda27@gmail.com>
2023-03-02 20:27:15 +0530

    :zap: Step-4: 'Data Splitting' done.

--- NVIDIA_Self-driving-car_simulation/learningFromSimulation.py
+++ NVIDIA_Self-driving-car_simulation/learningFromSimulation.py
@@ -1,4 +1,6 @@
 import utils
+from sklearn.model_selection import train_test_split
+
 
 # STEP-1: Load the data
 path = 'simulation_data'        # path of the data w.r.t. this file.
@@ -11,3 +13,9 @@ data = utils.balanceTrainingData(data, visualize_process=False)
 # STEP-3: Processing
 imgPaths, steeringAngles = utils.loadData(path, data)
 # print(imgPaths[0], steeringAngles[0])
+
+# STEP-4: Data splitting(into training and validation)
+xTrain, xTest, yTrain, yTest = train_test_split(
+    imgPaths, steeringAngles, test_size=0.2, random_state=5)
+print("Training set size: ", len(xTrain))
+print("Test set size: ", len(xTest))
