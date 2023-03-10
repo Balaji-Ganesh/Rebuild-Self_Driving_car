67d8bdf7517363c689932b138e7bece790960fbe
Balaji-Ganesh <balganeshboda27@gmail.com>
2023-03-10 12:29:59 +0530

    :art: Updated code struture to use on RPi

--- object_detection/COCO_mobilenet_ssd/detector.py
+++ object_detection/COCO_mobilenet_ssd/detector.py
@@ -1,8 +1,5 @@
 import cv2
 
-# img = cv2.imread('lena.jpg')    # to work on saved image
-capture = cv2.VideoCapture(0)   # to work on real-time feed.
-
 # Load all the classes
 classes = []    # to store all the classes
 with open('labels.txt', 'rt') as file:
@@ -20,21 +17,38 @@ net.setInputScale(1.0/127.5)
 net.setInputMean((127.5, 127.5, 127.5))
 net.setInputSwapRB(True)
 
-while True:
-    _, img = capture.read()     # read the frames from camera
+def detectObjects(img, min_thresh=0.45, nms_thresh=0.2, draw_info=True, detectable_classes=[]):
     # Send image to model to get prediction.
     classIds, confidences, bounding_boxes = net.detect(
-        img, confThreshold=0.6)  # consider if confidence is >50%
+        img, confThreshold=min_thresh, nmsThreshold=nms_thresh)  # consider if confidence is >50%, nms: Non max supressor (to avoid overlapping) - lower the value, the better.
 
+    detection_info = []         # To return the info of objects detected.
     if len(bounding_boxes) > 0:   # When detected any object..
-        print("#objects detected: ", len(bounding_boxes))
+        # If user not passed any classes to be detected, then detect all classes, on which the model is trained.
+        if len(detectable_classes) == 0: detectable_classes = classes   
+        
         # Overlay the prediction on the image..
         for classId, confidence, bbox in zip(classIds, confidences, bounding_boxes):
-            cv2.rectangle(img, bbox, color=(0, 255, 0), thickness=2)
-            print("id: ", classId)
-            cv2.putText(img, classes[classId-1].upper(),
-                        (bbox[0]+10, bbox[1]+30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+            detected_class = classes[classId-1]
+            # Detect only the objects to be detected, not all, on which the model is trained
+            if detected_class in detectable_classes:    
+                detection_info.append((detected_class, bbox))
+                if draw_info:       # Draw only if required..
+                    cv2.rectangle(img, bbox, color=(0, 255, 0), thickness=2)
+                    # print("id: ", classId)
+                    cv2.putText(img, detected_class.upper(),
+                                (bbox[0]+5, bbox[1]+15), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 0), 2)
 
-    # Display output
-    cv2.imshow('Output', img)
-    cv2.waitKey(1)
+    return img, detection_info
+
+if __name__ == '__main__':
+    # img = cv2.imread('lena.jpg')    # to work on saved image
+    capture = cv2.VideoCapture(0)   # to work on real-time feed.
+
+    while True:
+        _, img = capture.read()     # read the frames from camera
+        img, detection_info = detectObjects(img, detectable_classes=['stop sign', 'traffic light', 'car', 'truck']) # !! Make sure that, classes names match with `labels.txt`
+        print(detection_info)
+        # Display output
+        cv2.imshow('Output', img)
+        cv2.waitKey(1)
